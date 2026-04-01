b175bd0ffe976a694598855263d07644d7439fcc
phuc145 <thienphuc1452004@gmail.com>
2026-04-02 00:02:12 +0700

    Change generation time in web_ban_dien_thoai.sql
    Updated the generation time in the SQL file.

--- web_ban_dien_thoai.sql
+++ web_ban_dien_thoai.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 13, 2026 at 04:32 AM
+-- Generation Time: Apr 01, 2026 at 07:00 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
